1c26a30f7cdd27212c3bfbd796c8e6a7faa4daa4
tyler <tduic@andrew.cmu.edu>
2019-11-20 18:55:40 -0500

    update 3, working on 7

--- env/challenge3.py
+++ env/challenge3.py
@@ -1,38 +1,15 @@
 from utils import readFile
-import string
+import string, re
 
 def challenge3(path):
     # Write your code here
     text = readFile(path, "rt")
 
     res = ''
-    for i in range(3, len(text)-3):
-        ans = ''
-        tmp = 0
-        for j in range(9):
-            tmp = i + j - 4
-            if tmp >= 0 and tmp < len(text):
-                ans += text[tmp]
+    matches = re.findall("[a-z][A-Z]{3}[a-z]{1}[A-Z]{3}[a-z]", text)
+    for match in matches:
+        res += match[4]
 
-        cnt = 0
-        for k in range(len(ans)):
-            if i == 0:
-                if (k == 3 or k == 7) and ans[k] not in string.ascii_lowercase: break
-                if (k != 3 and k != 7) and ans[k] not in string.ascii_uppercase: break
-            if i == len(text)-4:
-                if (k == 0 or k == 4) and ans[k] not in string.ascii_lowercase: break
-                if (k != 0 and k != 4) and ans[k] not in string.ascii_uppercase: break
-            else:
-                if (k == 0 or k == 4 or k == 8) and ans[k] not in string.ascii_lowercase: break
-                if (k != 0 and k != 4 and k != 8) and ans[k] not in string.ascii_uppercase: break
-            cnt += 1
-
-        if cnt == len(ans):
-            # res.append(ans)
-            if i == 0:
-                res += ans[3]
-            else:
-                res += ans[4]
     return res
 
 if __name__ == '__main__':
--- env/challenge7.py
+++ env/challenge7.py
@@ -1,1 +1,53 @@
-import PIL
+from PIL import Image
+import requests, string
+from io import BytesIO
+
+def challenge7(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    data = list(img.getdata())
+    gray = []
+    i = 0
+    y = data[0][0]
+    s = ''
+    print(string.printable)
+    for p in data:
+        if (p[0] == p[1] == p[2]) and (y == p[0]):
+            i+=1
+        else:
+            if chr(y) in string.printable:
+                s += chr(y)*((i+2)//6)
+        i = 0
+        y = p[0]
+    print(s)
+
+
+    # i = 0
+    # y = data[0][0]
+    # s = ''
+    # for x in data:
+    #     if (x[0] == x[1] == x[2]) and (y == x[0]): #a grey pixel and same as last pixel
+    #         i+=1
+    #     else:
+    #         if chr(y) in (string.printable):
+    #             s+=chr(y)*((i+2)//6) # make every dot 6 pixels wide
+    #                                 # and show first only 4 pixels wide ‘s’ dot
+    #     i = 0
+    #     y = x[0]
+    #     print(s)
+    # bytestr = img.tobytes()
+    # res = ''
+    # for b in bytestr:
+    #     c = chr(b)
+    #     if c in string.ascii_letters:
+    #         res += c
+    # ans = ''
+    # for i in range(len(res)-4):
+    #     if res[i] == res[i+1] and res[i] == res[i+2] and res[i] == res[i+3]:
+    #         ans += res[i]
+    return None
+
+
+if __name__ == '__main__':
+    url = "http://www.pythonchallenge.com/pc/def/oxygen.png"
+    print(challenge7(url))
