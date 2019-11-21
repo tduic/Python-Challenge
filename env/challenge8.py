9a4c51fbef9cb3a3a7e3127d92bc4ae7fa325cc4
tyler <tduic@andrew.cmu.edu>
2019-11-20 21:10:50 -0500

    finished 8

--- env/challenge8.py
+++ env/challenge8.py
@@ -1,8 +1,19 @@
-from utils import readFile
+import requests
+import re
+from lxml import html
+import codecs
+import bz2
 
-def challenge8():
-    text = readFile("txt/challenge8.txt", "rt").split('\n')
-    user, pword = text[0], text[1]
+def challenge8(url):
+    response = requests.get(url)
+    resp = response.content
+    comment = codecs.escape_decode(re.findall(b"<!--(.*?)-->", resp, flags = re.DOTALL)[0])[0]
+    un = re.findall(b"\nun: '(.*?)'", comment)[0]
+    pw = re.findall(b"\npw: '(.*?)'", comment)[0]
+    user = bz2.decompress(un)
+    pWord = bz2.decompress(pw)
+    return (user, pWord)
 
 if __name__ == '__main__':
-    print(challenge8())
+    url = "http://www.pythonchallenge.com/pc/def/integrity.html"
+    print(challenge8(url))
